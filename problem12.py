496348d07a61792441e2cb76fd7727e877b7e698
catheyri <ian.cathey@gmail.com>
2015-10-12 15:43:33 -0700

    added functionality for generating list of numbers, and then a function for summing over a limited subset of those numbers

--- problem12.py
+++ problem12.py
@@ -1,1 +1,20 @@
 #Problem 12
+
+
+end = 10
+
+naturalNumbers = [x for x in range(1,end+1)]
+
+print naturalNumbers
+
+def triangle(nums, index):
+        newNums = nums[0:index]
+        sum = 0
+        for num in newNums:
+                print num
+                sum += num
+        return sum
+
+result = triangle(naturalNumbers, 3)
+print "\n"
+print result
