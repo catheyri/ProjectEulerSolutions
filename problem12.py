3b5d71fadfd580e008d8558534656a448ef177b2
catheyri <ian.cathey@gmail.com>
2015-10-12 16:47:49 -0700

    it's slow because i'm calculating the new triangle number every time, i should just add the new number and recompute

--- problem12.py
+++ problem12.py
@@ -1,35 +1,35 @@
 #Problem 12
 #dummy comment for test2
 
-end = 10
-
-naturalNumbers = [x for x in range(1,end+1)]
-
-print naturalNumbers
-
-def triangle(nums, index):
-        newNums = nums[0:index]
+def triangle(end):
+        newNums = [x for x in range(1,end+1)]
         sum = 0
         for num in newNums:
-                print num
                 sum += num
         return sum
 
-result = triangle(naturalNumbers, 7)
-print "\n"
-print result
-
 def factor(number):
         factors = []
-        for i in range(1,(number/2)+1):
+        for i in range(1,(number+1)):
                 if number%i == 0:
                         factors.append(i)
-                        number = number/i
-                        print i
         return factors
 
-primeFactors = factor(result)
-print primeFactors
-print len(primeFactors)
+numFactors = 0
+i = 1
+max = 1
+
+while 1:
+        triNum = triangle(i)            #Calculate the next triangular number
+        factors = factor(triNum)        #Factor that number
+        if len(factors) > max:
+                max = len(factors)
+                print str(max) + " (" + str(i) + "th triangle number, which is " + str(triNum) + ")"
+        if len(factors) > 500:          #How many factors are there? if >500, we are done
+                break
+        i += 1
+
+print triNum
+
         
                         
