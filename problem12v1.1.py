6278866d2a0e91b190d2eeba55c526c7a0d52a5b
catheyri <ian.cathey@gmail.com>
2015-10-12 17:51:35 -0700

    added some comments to v1.1

--- problem12v1.1.py
+++ problem12v1.1.py
@@ -1,1 +1,15 @@
 #Problem 12 - second try
+
+number = 300
+
+def factor(num):
+    divisors = []
+    for i in range(1,num+1):
+        if num%i == 0:
+            divisors.append(i)
+    return divisors
+
+print factor(number)
+
+#Turns out you only need to search for the factors of the sqrt and then multiply
+#that number by 2, (minus 1 if it is a perfect square)
